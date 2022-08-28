43a503d81c585f0578381b27791718bdb0d6e19f
Kwadwo-900 <sagudze900@gmail.com>
2022-08-28 02:03:54 +0000

    user management completed

--- UserManager.Designer.cs
+++ UserManager.Designer.cs
@@ -30,20 +30,25 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contact = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.roleBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.npass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.aname = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.dname = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -63,15 +68,15 @@
             this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.contact);
+            this.groupBox1.Controls.Add(this.email);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.roleBox);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.npass);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.aname);
             this.groupBox1.ForeColor = System.Drawing.Color.Olive;
             this.groupBox1.Location = new System.Drawing.Point(41, 55);
             this.groupBox1.Name = "groupBox1";
@@ -81,67 +86,37 @@
             this.groupBox1.Text = "Add new Users";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // panel1
+            // label6
             // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(933, 37);
-            this.panel1.TabIndex = 2;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "email";
             // 
-            // textBox1
+            // label5
             // 
-            this.textBox1.Location = new System.Drawing.Point(20, 79);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(252, 21);
-            this.textBox1.TabIndex = 0;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "contact";
             // 
-            // label2
+            // contact
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 60);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Assign Username";
+            this.contact.Location = new System.Drawing.Point(20, 250);
+            this.contact.Name = "contact";
+            this.contact.Size = new System.Drawing.Size(252, 21);
+            this.contact.TabIndex = 8;
             // 
-            // label3
+            // email
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 116);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 16);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Assign Password";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(20, 135);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(252, 21);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 16);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Assign Role";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "ADMIN",
-            "ATTENDANT"});
-            this.comboBox1.Location = new System.Drawing.Point(20, 190);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 5;
+            this.email.Location = new System.Drawing.Point(20, 306);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(252, 21);
+            this.email.TabIndex = 7;
             // 
             // button1
             // 
@@ -156,38 +131,113 @@
             this.button1.Text = "Add user";
             this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox3
+            // roleBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(20, 306);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(252, 21);
-            this.textBox3.TabIndex = 7;
+            this.roleBox.FormattingEnabled = true;
+            this.roleBox.Items.AddRange(new object[] {
+            "ADMIN",
+            "ATTENDANT"});
+            this.roleBox.Location = new System.Drawing.Point(20, 190);
+            this.roleBox.Name = "roleBox";
+            this.roleBox.Size = new System.Drawing.Size(121, 24);
+            this.roleBox.TabIndex = 5;
             // 
-            // textBox4
+            // label4
             // 
-            this.textBox4.Location = new System.Drawing.Point(20, 250);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(252, 21);
-            this.textBox4.TabIndex = 8;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Assign Role";
             // 
-            // label5
+            // label3
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 231);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 16);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "contact";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Assign Password";
             // 
-            // label6
+            // npass
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(20, 290);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(36, 16);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "email";
+            this.npass.Location = new System.Drawing.Point(20, 135);
+            this.npass.Name = "npass";
+            this.npass.Size = new System.Drawing.Size(252, 21);
+            this.npass.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Assign Username";
+            // 
+            // aname
+            // 
+            this.aname.Location = new System.Drawing.Point(20, 79);
+            this.aname.Name = "aname";
+            this.aname.Size = new System.Drawing.Size(252, 21);
+            this.aname.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(933, 37);
+            this.panel1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.dname);
+            this.groupBox2.ForeColor = System.Drawing.Color.Olive;
+            this.groupBox2.Location = new System.Drawing.Point(412, 55);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(352, 227);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Remove User";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Olive;
+            this.button2.Location = new System.Drawing.Point(88, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Remove User";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(47, 92);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 16);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Enter Username";
+            // 
+            // dname
+            // 
+            this.dname.Location = new System.Drawing.Point(50, 111);
+            this.dname.Name = "dname";
+            this.dname.Size = new System.Drawing.Size(252, 21);
+            this.dname.TabIndex = 0;
             // 
             // UserManager
             // 
@@ -195,6 +245,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -206,6 +257,8 @@
             this.groupBox1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -216,15 +269,19 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox aname;
+        private System.Windows.Forms.ComboBox roleBox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox npass;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox contact;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox dname;
     }
 }
--- UserManager.cs
+++ UserManager.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Super_Market_Inventory
 {
@@ -17,9 +18,66 @@ namespace Super_Market_Inventory
             InitializeComponent();
         }
 
+        public static string connString = "Data Source=SILASPC\\SQLEXPRESS;Initial Catalog=mktdb;Integrated Security=True";
+
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(aname.Text == "" || npass.Text == "" || contact.Text == "" || email.Text =="" )
+            {
+                MessageBox.Show("Enter the full details of the user!! ");
+            }
+
+            try
+            {
+                SqlCommand command;
+                SqlConnection conn = new SqlConnection(connString);
+
+
+                string sql = "INSERT INTO user_table VALUES('" + this.aname.Text + "','" + this.npass.Text + "', '" + this.roleBox.Text + "', '" + this.contact.Text + "', '" + this.email.Text + "') ";
+                command = new SqlCommand(sql,conn);
+                conn.Open();
+                command.ExecuteNonQuery();
+                MessageBox.Show("New User Addded Successfully!!");
+                aname.Clear();
+                npass.Clear();
+                roleBox.ResetText();
+                contact.Clear();
+                email.Clear();
+
+                conn.Close();
+             }
+            catch(Exception es)
+            {
+                MessageBox.Show(es + " error occured!!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try { 
+            SqlCommand command;
+            SqlConnection conn = new SqlConnection(connString);
+
+            command = new SqlCommand("DELETE FROM user_table WHERE uname = '" + dname.Text + "'", conn);
+            conn.Open();
+            command.ExecuteNonQuery();
+
+            MessageBox.Show("User removed successfully");
+                dname.Clear();
+
+                conn.Close();
+                }
+            catch(Exception es)
+            {
+                MessageBox.Show(es + " error occured!!!");
+                
+            }
         }
     }
 }
